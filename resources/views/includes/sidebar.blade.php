6735e92e4abfbf95619e5736fe0c14826fe870d5
arief budiman <ariefbudimann20@gmail.com>
2020-04-23 21:35:20 +0700

    update terbaru

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,63 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="admin.html" class="brand-link">
+      <i class="ml-3 fas fa-box-open"></i>
+      <span class="brand-text font-weight-light">Sumbang Saran</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          
+          <li class="nav-header">JOB ADMIN</li>
+          <li class="nav-item">
+            <a href="admin-sumbangsaran.html" class="nav-link">
+              <i class="nav-icon fab fa-audible"></i>
+              <p>Kelola Sumbang Saran</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="admin-datakaryawan.html" class="nav-link">
+              <i class="nav-icon fas fa-database"></i>
+              <p>Data Karyawan</p>
+            </a>
+          </li>
+
+          <li class="nav-header">JOB TIM PENILAI</li>
+          <li class="nav-item">
+            <a href="admin-penilaian.html" class="nav-link">
+              <i class="nav-icon fas fa-star-half-alt"></i>
+              <p>Penilaian</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="admin-pesertaterbaik" class="nav-link">
+              <i class="nav-icon fas fa-medal"></i>
+              <p>Peserta Terbaik</p>
+            </a>
+          </li>
+
+          <li class="nav-header">AKUN</li>
+          <li class="nav-item">
+            <a href="login.html" class="nav-link">
+              <i class="fas fa-sign-out-alt"></i>
+              <p>Keluar</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
