4e77f5bfe1704651ec20c5b9cd9f5b4721cfad1a
rfahlevi <rezafahlevi55@gmail.com>
2020-04-26 16:02:57 +0700

    Date picker modal ubah periode

--- resources/views/pages/sumbang-saran/index.blade.php
+++ resources/views/pages/sumbang-saran/index.blade.php
@@ -15,10 +15,46 @@
               </ol>
             </div><!-- /.col -->
           </div><!-- /.row -->
+          <div class="row topbar-ss">
+            <div class="col-12">
+                <button class="btn btn-warning float-left ubah-periode" data-toggle="modal" data-target="#modal-ubahperiode"><i class="fas fa-clock"></i> Ubah Periode</button>    
+            </div>        
+        </div>
         </div><!-- /.container-fluid -->
       </div>
       <!-- /.content-header -->
-    
+
+      <!-- Modal Ubah Periode -->
+      <div class="modal fade modal-ubah-periode" id="modal-ubahperiode" tabindex="-1" role="dialog" aria-labelledby="modal-ubahperiodeTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="modal-ubahperiodeTitle">Periode Sumbang Saran</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form action="">
+                  <div class="form-group">
+                      <label for="mulai">Tanggal Mulai &nbsp;&nbsp;:</label>
+                      <input type="text" id="mulai" class="date-picker">
+                  </div>
+                  <div class="form-group">
+                      <label for="selesai">Tanggal Selesai :</label>
+                      <input type="text" id="selesai" class="date-picker">
+                  </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal Ubah Periode -->
+
     <section class="content">
         <div class="row">
             <div class="col-12">
@@ -46,7 +82,7 @@
                                         <td>{{$ss->judul}}</td>
                                         <td>{{$ss->bagian}}</td>
                                         <td>
-                                            <a href="" class="btn btn-info" role="button" data-toggle="tooltip" data-placement="top" title="Detail">
+                                            <a href="" class="btn btn-info" role="button" data-toggle="modal" data-target="#detailss-modal">
                                                 <i class="fas fa-eye"></i>
                                             </a>
                                             <form action="{{url('sumbang-saran',$ss->id)}}" method="POST" class="d-inline">
@@ -61,6 +97,64 @@
                                     @endforeach
                                 </tbody>
                             </table>
+                        </div>
+
+                        <div class="modal fade" id="detailss-modal" tabindex="-1" role="dialog" aria-labelledby="detailss-modalTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="detailss-modalTitle">Detail Sumbang Saran</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <table class="table table-bordered">
+                                            @foreach ($sumbangsaran as $ss)
+                                            <tr>
+                                                <th width="250px;">NIK</th>
+                                                <td>{{$ss->nik}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Nama Lengkap</th>
+                                                <td>{{$ss->nama}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Bagian</th>
+                                                <td>{{$ss->bagian}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Ext</th>
+                                                <td>{{$ss->ext}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Judul Sumbang Saran</th>
+                                                <td>{{$ss->judul}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Foto</th>
+                                                <td>{{$ss->foto}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Kondisi Awal</th>
+                                                <td>{{$ss->kondisi_awal}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Kondisi Yang Diinginkan</th>
+                                                <td>{{$ss->kondisi_akhir}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Manfaat Bagi Perusahaan</th>
+                                                <td>{{$ss->manfaat}}</td>
+                                            </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                      </div>                                
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -79,3 +173,15 @@
         } );
     </script>
 @endpush
+
+{{-- Date Picker --}}
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="{{url('assets/plugins/jquery-ui/jquery-ui.css')}}">
+
+    <script>
+        $(document).ready(function(){
+            $('.date-picker').datepicker();
+        });
+    </script>
+
