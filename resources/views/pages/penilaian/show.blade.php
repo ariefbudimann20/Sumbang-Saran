2926c0cc4594c8b460bd0babff656e91cf15dd67
rfahlevi <rezafahlevi55@gmail.com>
2020-04-27 12:01:48 +0700

    Fix Modal Sumbang saran & Penilaian

--- resources/views/pages/penilaian/show.blade.php
+++ resources/views/pages/penilaian/show.blade.php
@@ -1,52 +1,74 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 col-sm-12">
             <div class="form-group">
                 <label>Nomor Induk Karyawan : </label>
                 <p>{{$ss->nik}}</p>
             </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
             <div class="form-group">
                 <label>Nama Lengkap :</label>
                 <p>{{$ss->nama}}</p>
             </div>
-            <div class="form-group">
-                <label>Judul Sumbang Saran :</label>
-                <p>{{$ss->judul}}</p>
-            </div>
-            <div class="form-group">
-                <label>Gambarkan Kondisi Awal :</label>
-                <p>{{$ss->kondisi_awal}}</p>
-            </div>
-            <div class="form-group">
-                <label>Gambarkan Kondisi Yang Diinginkan :</label>
-                <p>{{$ss->kondisi_akhir}}</p>
-            </div>  
         </div>
-        <div class="col-md-6">
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
             <div class="form-group">
                 <label>Bagian :</label>
                 <p>{{$ss->bagian}}</p>
             </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
             <div class="form-group">
                 <label>Ext :</label>
                 <p>{{$ss->ext}}</p>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <div class="form-group">
+                <label>Judul Sumbang Saran :</label>
+                <p>{{$ss->judul}}</p>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
             <div class="form-group">
                 <label for="foto">Foto :</label><br>
                 <div class="col-md-6 col-lg-4 item zoom-on-hover"><a href="{{url('assets/images',$ss->foto)}}" data-lightbox="image-1">
                     <img style="width:auto; height:80px; margin-bottom:10px;" src="{{url('assets/images',$ss->foto)}}" /></a>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <div class="form-group">
+                <label>Gambarkan Kondisi Awal :</label>
+                <p class="text-justify">{{$ss->kondisi_awal}}</p>
+            </div>  
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <div class="form-group">
+                <label>Gambarkan Kondisi Yang Diinginkan :</label>
+                <p class="text-justify">{{$ss->kondisi_akhir}}</p>
+            </div>  
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12">
             <div class="form-group">
                 <label>Manfaat Bagi Perusahaan :</label>
-                <p>{{$ss->manfaat}}</p>
+                <p class="text-justify">{{$ss->manfaat}}</p>
             </div>
         </div>
     </div>
     <div class="row border border-info bg-penilaian">
         <div class="col-12 pt-4">
-            <h3 class="mb-4">FORM PENILAIAN</h3>
+            <h4 class="mb-2 ml-2 font-italic">FORM PENILAIAN</h4>
         </div>
         <div class="col-md-6 col-sm-12">
             <pre>
